0c139e5e343defbda7301b8d07833092f2dd2fb0
Navaltechzure3 <navaltechzure2@outlook.com>
2021-07-31 09:45:43 +0000

    v9

--- getip.bash
+++ getip.bash
@@ -5,9 +5,4 @@ echo $pub_ib
 echo "ip added in inventory successfully"
 echo "Copying ssh key to new machine"
 sshpass -p Efb2021!2@3# ssh-copy-id  vmadmin@`cat /home/vmadmin/ip`
-    if [ $? -eq 0 ]; then
-        echo "Copied key successfully "
-    else
-        echo "key copying failed"
-    fi
 echo "done"
