26c06aaadc532917a6c427fd60b56a551162c11c
Navaltechzure3 <navaltechzure2@outlook.com>
2021-07-31 13:45:44 +0000

    v9

--- getip.bash
+++ getip.bash
@@ -1,8 +1,8 @@
 #/bin/bash
-az vm show -d -g ansibleRG -n ansiblevm --query publicIps -o tsv > /home/vmadmin/ip
+#az vm show -d -g ansibleRG -n ansiblevm --query publicIps -o tsv > /home/vmadmin/ip
 pub_ib=`cat /home/vmadmin/ip`
 echo $pub_ib
 echo "ip added in inventory successfully"
 echo "Copying ssh key to new machine"
-sshpass -p Efb2021!2@3# ssh-copy-id  vmadmin@`cat /home/vmadmin/ip`
+sshpass -p Efb2021!2@3# ssh-copy-id  vmadmin@$pub_ip
 echo "done"
