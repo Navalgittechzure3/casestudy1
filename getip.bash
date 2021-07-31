659d0e19301f80f37fc3251c91982d15a6faf5bb
Navaltechzure3 <navaltechzure2@outlook.com>
2021-07-31 08:48:31 +0000

    v6

--- getip.bash
+++ getip.bash
@@ -5,5 +5,5 @@ pub_ib=`cat /home/vmadmin/ip`
 echo $pub_ib
 echo "ip added in inventory successfully"
 echo "Copying ssh key to new machine"
-sshpass -p Efb2021!2@3# ssh-copy-id  vmadmin@${pubip}
+sshpass -p Efb2021!2@3# ssh-copy-id  vmadmin@${pub_ip}
 echo "Copied key successfully " 
