cbe1a7cc538c1aab5df2f7dea1baaf6040b88741
Navaltechzure3 <navaltechzure2@outlook.com>
2021-08-02 18:03:17 +0000

    sshkeygen+

--- getip.bash
+++ getip.bash
@@ -1,5 +1,6 @@
 #/bin/bash
 #az vm show -d -g ansibleRG -n ansiblevm --query publicIps -o tsv > /home/vmadmin/ip
+ssh-keygen -q -t rsa -N '' <<< ""$'\n'"y" >/dev/null 2>&1
 pub_ib=`cat /home/vmadmin/ip`
 echo $pub_ib
 echo "ip added in inventory successfully"
